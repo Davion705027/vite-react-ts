901ce57912973753dea512f581dc750d21b8746e
davion <pmtydavion>
2023-12-31 18:41:44 +0800

    redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,24 @@ import { useState } from 'react'
 import './App.css'
 import { Button, Card } from 'antd'
 import Login from './pages/login'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from './store/features/counterSlice'
 
 function App() {
   const [count, setCount] = useState(0)
+   // 通过useSelector直接拿到store中定义的value
+   const {value} = useSelector((store:any)=>store.counter)
+   // 通过useDispatch 派发事件
+   const dispatch = useDispatch()
 
   return (
     <>
       <Card>
         <Login></Login>  
       </Card>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <p>{value}</p>
+        <button onClick={()=>{dispatch(increment())}}>加</button>
+        <button onClick={()=>{dispatch(decrement())}}>减</button>
     </>
   )
 }
